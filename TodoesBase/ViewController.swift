54b4de25d447d3837d23c901580693fbe6201ea1
Marcos Felipe <mfelipesp@gmail.com>
2019-10-30 17:33:48 -0300

    Initial Commit

---
+++ TodoesBase/ViewController.swift
@@ -0,0 +1,90 @@
+//
+//  ViewController.swift
+//  TodoesBase
+//
+//  Created by Marcos Felipe Souza on 30/10/19.
+//  Copyright © 2019 Marcos Felipe Souza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    private let cellID = "cellReuse"
+    
+    let stack = CoreDataStack.shared
+    
+    var items: [Item] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        searchBar.delegate = self
+        tableView.dataSource = self
+        tableView.delegate = self
+        loadDatas()
+    }
+    
+    @IBAction func addItem(_ sender: UIBarButtonItem) {
+        
+        let alert = UIAlertController(title: "Adicione um novo Item",
+                                      message: "",
+                                      preferredStyle: .alert)
+        
+        var textField = UITextField()
+        alert.addTextField { tf in
+            textField = tf
+            textField.placeholder = "Digite"
+        }
+        
+        alert.addAction(UIAlertAction(title: "Salvar", style: .default, handler: { (alert) in
+            
+            let item = Item(context: self.stack.managedContext)
+            item.name = textField.text
+            self.stack.saveContext()
+            self.loadDatas()
+            
+        }))
+        alert.addAction(UIAlertAction(title: "Cancelar", style: .cancel))
+        self.present(alert, animated: true)
+    }
+    
+    func loadDatas() {
+        do {
+            self.items = try stack.managedContext.fetch(Item.fetchRequest())
+            self.tableView.reloadData()
+        } catch {
+            print("Error on ViewDidLoad on Load of Items")
+        }
+    }
+    
+}
+
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: self.cellID, for: indexPath)
+        let item = items[indexPath.row]
+        cell.textLabel?.text = item.name
+        cell.accessoryType = item.done ? .checkmark : .none
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.performBatchUpdates({
+            let item = self.items[indexPath.row]
+            item.done = !item.done
+            stack.saveContext()
+        })
+        tableView.reloadRows(at: [indexPath], with: .left)
+        
+    }
+}
+
+extension ViewController: UISearchBarDelegate {
+    
+}
